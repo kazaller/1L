d71ecfe7ede9868fe8c88eaa113cfdbb6b910bb1
kazaller <35352091+kazaller@users.noreply.github.com>
2018-04-23 15:50:01 -0700

    Update proc.h

--- xv6/proc.h
+++ xv6/proc.h
@@ -49,7 +49,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int exitstatus; // CS 153
 };
 
 // Process memory is laid out contiguously, low addresses first:
