43f8e5e89dde44f639a2ae411b32f610ea13763f
qijieguan <qijieguan7@gmail.com>
2018-05-10 10:49:13 -0700

    Update proc.h

--- xv6/proc.h
+++ xv6/proc.h
@@ -50,6 +50,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int exitstatus; // CS 153
+  int priority; // CS 153
 };
 
 // Process memory is laid out contiguously, low addresses first:
